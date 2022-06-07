dfd188ef193bc75debeac329458d21140c657aad
arifintajul4 <arifintajul4@gmail.com>
2022-06-07 14:01:23 +0700

    add video

--- src/App.js
+++ src/App.js
@@ -9,177 +9,34 @@ import React, { Fragment, useState } from "react";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <div className="md:container px-4 md:px-auto">
+      <div className="md:container px-4 md:px-auto pb-[10rem] min-h-screen">
         <h1 className="font-bold text-xl md:text-4xl my-4">Our Product</h1>
         <hr />
         <div className="grid grid-cols-2 md:grid-cols-3 gap-2 md:gap-5 mt-4">
           <div className="relative">
-            <div className="w-full max-w-sm bg-white space-y-3 hover:shadow-xl hover:border hover:p-4 p-2 pb-4 group hover:absolute hover:-left-[0%] hover:z-10 hover:-top-[1%]">
-              <img alt="Sample Car" src="static/images/sample-car.jpg" />
+            <div className="w-full transition-all duration-500 max-w-sm bg-white space-y-3 hover:shadow-xl hover:border hover:p-4 p-2 pb-4 group hover:absolute hover:-left-[0%] hover:z-10 hover:-top-[1%]">
+              <img
+                alt="Sample Car"
+                src="https://cdn.tmrw.com.sg/pml-pma/dist/img/img-pml-bmw-autoverse-1-series.jpg?r2"
+              />
               <h1 className="md:text-2xl text-lg font-bold">BMW 1 Series</h1>
               <p className="md:text-base text-sm">
                 Take control with modern dynamics.
               </p>
               <div className="group-hover:flex gap-2 hidden">
-                <a
-                  href="https://plonk.revezdigital.com/production/instant/?id=44d50aa3-e1df-ec11-ab22-06dd318c68b0"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="bg-blue-500 md:text-base text-xs text-white uppercase px-3 py-2"
-                >
-                  Discover Now
-                </a>
                 <button
-                  onClick={() => setIsOpen(true)}
-                  className="bg-gray-500 md:text-base text-xs text-white uppercase px-3 py-2"
-                >
-                  Scan Now
-                </button>
-              </div>
-              <div className="group-hover:flex gap-2 hidden">
-                <h1 className="font-semibold">Share Now</h1>
-                <a href="">
-                  <FontAwesomeIcon icon={faFacebook} />
-                </a>
-                <a href="">
-                  <FontAwesomeIcon icon={faYoutube} />
-                </a>
-                <a href="">
-                  <FontAwesomeIcon icon={faTwitter} />
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="relative">
-            <div className="w-full max-w-sm bg-white space-y-3 hover:shadow-xl hover:border hover:p-4 p-2 pb-4 group hover:absolute hover:-left-[0%] hover:z-10 hover:-top-[1%]">
-              <img alt="Sample Car" src="static/images/sample-car.jpg" />
-              <h1 className="md:text-2xl text-lg font-bold">BMW 1 Series</h1>
-              <p className="md:text-base text-sm">
-                Take control with modern dynamics.
-              </p>
-              <div className="group-hover:flex gap-2 hidden">
-                <a
-                  href="https://plonk.revezdigital.com/production/instant/?id=44d50aa3-e1df-ec11-ab22-06dd318c68b0"
-                  target="_blank"
-                  rel="noreferrer"
+                  onClick={() => {
+                    setShowModal(true);
+                    document.body.style.overflow = "hidden";
+                  }}
                   className="bg-blue-500 md:text-base text-xs text-white uppercase px-3 py-2"
                 >
                   Discover Now
-                </a>
-                <button
-                  onClick={() => setIsOpen(true)}
-                  className="bg-gray-500 md:text-base text-xs text-white uppercase px-3 py-2"
-                >
-                  Scan Now
                 </button>
-              </div>
-              <div className="group-hover:flex gap-2 hidden">
-                <h1 className="font-semibold">Share Now</h1>
-                <a href="">
-                  <FontAwesomeIcon icon={faFacebook} />
-                </a>
-                <a href="">
-                  <FontAwesomeIcon icon={faYoutube} />
-                </a>
-                <a href="">
-                  <FontAwesomeIcon icon={faTwitter} />
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="relative">
-            <div className="w-full max-w-sm bg-white space-y-3 hover:shadow-xl hover:border hover:p-4 p-2 pb-4 group hover:absolute hover:-left-[0%] hover:z-10 hover:-top-[1%]">
-              <img alt="Sample Car" src="static/images/sample-car.jpg" />
-              <h1 className="md:text-2xl text-lg font-bold">BMW 1 Series</h1>
-              <p className="md:text-base text-sm">
-                Take control with modern dynamics.
-              </p>
-              <div className="group-hover:flex gap-2 hidden">
-                <a
-                  href="https://plonk.revezdigital.com/production/instant/?id=44d50aa3-e1df-ec11-ab22-06dd318c68b0"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="bg-blue-500 md:text-base text-xs text-white uppercase px-3 py-2"
-                >
-                  Discover Now
-                </a>
-                <button
-                  onClick={() => setIsOpen(true)}
-                  className="bg-gray-500 md:text-base text-xs text-white uppercase px-3 py-2"
-                >
-                  Scan Now
-                </button>
-              </div>
-              <div className="group-hover:flex gap-2 hidden">
-                <h1 className="font-semibold">Share Now</h1>
-                <a href="">
-                  <FontAwesomeIcon icon={faFacebook} />
-                </a>
-                <a href="">
-                  <FontAwesomeIcon icon={faYoutube} />
-                </a>
-                <a href="">
-                  <FontAwesomeIcon icon={faTwitter} />
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="relative">
-            <div className="w-full max-w-sm bg-white space-y-3 hover:shadow-xl hover:border hover:p-4 p-2 pb-4 group hover:absolute hover:-left-[0%] hover:z-10 hover:-top-[1%]">
-              <img alt="Sample Car" src="static/images/sample-car.jpg" />
-              <h1 className="md:text-2xl text-lg font-bold">BMW 1 Series</h1>
-              <p className="md:text-base text-sm">
-                Take control with modern dynamics.
-              </p>
-              <div className="group-hover:flex gap-2 hidden">
-                <a
-                  href="https://plonk.revezdigital.com/production/instant/?id=44d50aa3-e1df-ec11-ab22-06dd318c68b0"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="bg-blue-500 md:text-base text-xs text-white uppercase px-3 py-2"
-                >
-                  Discover Now
-                </a>
-                <button
-                  onClick={() => setIsOpen(true)}
-                  className="bg-gray-500 md:text-base text-xs text-white uppercase px-3 py-2"
-                >
-                  Scan Now
-                </button>
-              </div>
-              <div className="group-hover:flex gap-2 hidden">
-                <h1 className="font-semibold">Share Now</h1>
-                <a href="">
-                  <FontAwesomeIcon icon={faFacebook} />
-                </a>
-                <a href="">
-                  <FontAwesomeIcon icon={faYoutube} />
-                </a>
-                <a href="">
-                  <FontAwesomeIcon icon={faTwitter} />
-                </a>
-              </div>
-            </div>
-          </div>
-          <div className="relative">
-            <div className="w-full max-w-sm bg-white space-y-3 hover:shadow-xl hover:border hover:p-4 p-2 pb-4 group hover:absolute hover:-left-[0%] hover:z-10 hover:-top-[1%]">
-              <img alt="Sample Car" src="static/images/sample-car.jpg" />
-              <h1 className="md:text-2xl text-lg font-bold">BMW 1 Series</h1>
-              <p className="md:text-base text-sm">
-                Take control with modern dynamics.
-              </p>
-              <div className="group-hover:flex gap-2 hidden">
-                <a
-                  href="https://plonk.revezdigital.com/production/instant/?id=44d50aa3-e1df-ec11-ab22-06dd318c68b0"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="bg-blue-500 md:text-base text-xs text-white uppercase px-3 py-2"
-                >
-                  Discover Now
-                </a>
                 <button
                   onClick={() => setIsOpen(true)}
                   className="bg-gray-500 md:text-base text-xs text-white uppercase px-3 py-2"
@@ -203,20 +60,185 @@ function App() {
           </div>
           <div className="relative">
             <div className="w-full transition-all duration-500 max-w-sm bg-white space-y-3 hover:shadow-xl hover:border hover:p-4 p-2 pb-4 group hover:absolute hover:-left-[0%] hover:z-10 hover:-top-[1%]">
-              <img alt="Sample Car" src="static/images/sample-car.jpg" />
+              <img
+                alt="Sample Car"
+                src="https://cdn.tmrw.com.sg/pml-pma/dist/img/img-pml-bmw-autoverse-1-series.jpg?r2"
+              />
               <h1 className="md:text-2xl text-lg font-bold">BMW 1 Series</h1>
               <p className="md:text-base text-sm">
                 Take control with modern dynamics.
               </p>
               <div className="group-hover:flex gap-2 hidden">
-                <a
-                  href="https://plonk.revezdigital.com/production/instant/?id=44d50aa3-e1df-ec11-ab22-06dd318c68b0"
-                  target="_blank"
-                  rel="noreferrer"
+                <button
+                  onClick={() => {
+                    setShowModal(true);
+                    document.body.style.overflow = "hidden";
+                  }}
                   className="bg-blue-500 md:text-base text-xs text-white uppercase px-3 py-2"
                 >
                   Discover Now
+                </button>
+                <button className="bg-gray-500 md:text-base text-xs text-white uppercase px-3 py-2">
+                  Scan Now
+                </button>
+              </div>
+              <div className="group-hover:flex gap-2 hidden">
+                <h1 className="font-semibold">Share Now</h1>
+                <a href="">
+                  <FontAwesomeIcon icon={faFacebook} />
                 </a>
+                <a href="">
+                  <FontAwesomeIcon icon={faYoutube} />
+                </a>
+                <a href="">
+                  <FontAwesomeIcon icon={faTwitter} />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="relative">
+            <div className="w-full transition-all duration-500 max-w-sm bg-white space-y-3 hover:shadow-xl hover:border hover:p-4 p-2 pb-4 group hover:absolute hover:-left-[0%] hover:z-10 hover:-top-[1%]">
+              <img
+                alt="Sample Car"
+                src="https://cdn.tmrw.com.sg/pml-pma/dist/img/img-pml-bmw-autoverse-1-series.jpg?r2"
+              />
+              <h1 className="md:text-2xl text-lg font-bold">BMW 1 Series</h1>
+              <p className="md:text-base text-sm">
+                Take control with modern dynamics.
+              </p>
+              <div className="group-hover:flex gap-2 hidden">
+                <button
+                  onClick={() => {
+                    setShowModal(true);
+                    document.body.style.overflow = "hidden";
+                  }}
+                  className="bg-blue-500 md:text-base text-xs text-white uppercase px-3 py-2"
+                >
+                  Discover Now
+                </button>
+                <button
+                  onClick={() => setIsOpen(true)}
+                  className="bg-gray-500 md:text-base text-xs text-white uppercase px-3 py-2"
+                >
+                  Scan Now
+                </button>
+              </div>
+              <div className="group-hover:flex gap-2 hidden">
+                <h1 className="font-semibold">Share Now</h1>
+                <a href="">
+                  <FontAwesomeIcon icon={faFacebook} />
+                </a>
+                <a href="">
+                  <FontAwesomeIcon icon={faYoutube} />
+                </a>
+                <a href="">
+                  <FontAwesomeIcon icon={faTwitter} />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="relative">
+            <div className="w-full transition-all duration-500 max-w-sm bg-white space-y-3 hover:shadow-xl hover:border hover:p-4 p-2 pb-4 group hover:absolute hover:-left-[0%] hover:z-10 hover:-top-[1%]">
+              <img
+                alt="Sample Car"
+                src="https://cdn.tmrw.com.sg/pml-pma/dist/img/img-pml-bmw-autoverse-1-series.jpg?r2"
+              />
+              <h1 className="md:text-2xl text-lg font-bold">BMW 1 Series</h1>
+              <p className="md:text-base text-sm">
+                Take control with modern dynamics.
+              </p>
+              <div className="group-hover:flex gap-2 hidden">
+                <button
+                  onClick={() => {
+                    setShowModal(true);
+                    document.body.style.overflow = "hidden";
+                  }}
+                  className="bg-blue-500 md:text-base text-xs text-white uppercase px-3 py-2"
+                >
+                  Discover Now
+                </button>
+                <button
+                  onClick={() => setIsOpen(true)}
+                  className="bg-gray-500 md:text-base text-xs text-white uppercase px-3 py-2"
+                >
+                  Scan Now
+                </button>
+              </div>
+              <div className="group-hover:flex gap-2 hidden">
+                <h1 className="font-semibold">Share Now</h1>
+                <a href="">
+                  <FontAwesomeIcon icon={faFacebook} />
+                </a>
+                <a href="">
+                  <FontAwesomeIcon icon={faYoutube} />
+                </a>
+                <a href="">
+                  <FontAwesomeIcon icon={faTwitter} />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="relative">
+            <div className="w-full transition-all duration-500 max-w-sm bg-white space-y-3 hover:shadow-xl hover:border hover:p-4 p-2 pb-4 group hover:absolute hover:-left-[0%] hover:z-10 hover:-top-[1%]">
+              <img
+                alt="Sample Car"
+                src="https://cdn.tmrw.com.sg/pml-pma/dist/img/img-pml-bmw-autoverse-1-series.jpg?r2"
+              />
+              <h1 className="md:text-2xl text-lg font-bold">BMW 1 Series</h1>
+              <p className="md:text-base text-sm">
+                Take control with modern dynamics.
+              </p>
+              <div className="group-hover:flex gap-2 hidden">
+                <button
+                  onClick={() => {
+                    setShowModal(true);
+                    document.body.style.overflow = "hidden";
+                  }}
+                  className="bg-blue-500 md:text-base text-xs text-white uppercase px-3 py-2"
+                >
+                  Discover Now
+                </button>
+                <button
+                  onClick={() => setIsOpen(true)}
+                  className="bg-gray-500 md:text-base text-xs text-white uppercase px-3 py-2"
+                >
+                  Scan Now
+                </button>
+              </div>
+              <div className="group-hover:flex gap-2 hidden">
+                <h1 className="font-semibold">Share Now</h1>
+                <a href="">
+                  <FontAwesomeIcon icon={faFacebook} />
+                </a>
+                <a href="">
+                  <FontAwesomeIcon icon={faYoutube} />
+                </a>
+                <a href="">
+                  <FontAwesomeIcon icon={faTwitter} />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="relative">
+            <div className="w-full transition-all duration-500 max-w-sm bg-white space-y-3 hover:shadow-xl hover:border hover:p-4 p-2 pb-4 group hover:absolute hover:-left-[0%] hover:z-10 hover:-top-[1%]">
+              <img
+                alt="Sample Car"
+                src="https://cdn.tmrw.com.sg/pml-pma/dist/img/img-pml-bmw-autoverse-1-series.jpg?r2"
+              />
+              <h1 className="md:text-2xl text-lg font-bold">BMW 1 Series</h1>
+              <p className="md:text-base text-sm">
+                Take control with modern dynamics.
+              </p>
+              <div className="group-hover:flex gap-2 hidden">
+                <button
+                  onClick={() => {
+                    setShowModal(true);
+                    document.body.style.overflow = "hidden";
+                  }}
+                  className="bg-blue-500 md:text-base text-xs text-white uppercase px-3 py-2"
+                >
+                  Discover Now
+                </button>
                 <button
                   onClick={() => setIsOpen(true)}
                   className="bg-gray-500 md:text-base text-xs text-white uppercase px-3 py-2"
@@ -291,7 +313,40 @@ function App() {
           </Dialog>
         </Transition>
       </div>
-      <div className="w-full bg-black p-2 text-center text-white fixed bottom-0 left-0 right-0">
+      {showModal && (
+        <div className="fixed z-50 left-0 right-0 bottom-0 top-0 bg-black flex justify-center items-center">
+          <button
+            onClick={() =>
+              (window.location.href =
+                "https://plonk.revezdigital.com/production/instant?id=44d50aa3-e1df-ec11-ab22-06dd318c68b0")
+            }
+            className="fixed top-5 right-10 text-white border rounded-full px-4 opacity-80 hover:opacity-100"
+          >
+            SKIP VIDEO
+          </button>
+          <button
+            onClick={() => setShowModal(false)}
+            className="fixed bottom-10 z-[51] text-white border rounded-full px-4 opacity-80 hover:opacity-100"
+          >
+            CLOSE
+          </button>
+          <video
+            playsInline
+            autoPlay
+            onEnded={() =>
+              (window.location.href =
+                "https://plonk.revezdigital.com/production/instant?id=44d50aa3-e1df-ec11-ab22-06dd318c68b0")
+            }
+          >
+            <source
+              src="./react-ar-example/static/video/sample-video.mp4"
+              type="video/mp4"
+            />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      )}
+      <div className="w-full bg-black p-2 text-center text-white">
         Copyright © 2022 . All rights reserved.
       </div>
     </>
